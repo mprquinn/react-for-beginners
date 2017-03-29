2f9539c539f92af46e0fdb8a1427ed15892b0786
Mike <mprquinn@gmail.com>
2017-03-29 11:25:27 -0600

    remove from order button added

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,9 @@ class App extends React.Component {
 		this.addFish = this.addFish.bind(this);
 		this.loadSamples = this.loadSamples.bind(this);
 		this.addToOrder = this.addToOrder.bind(this);
+		this.removeFromOrder = this.removeFromOrder.bind(this);
+		this.updateFish = this.updateFish.bind(this);
+		this.removeFish = this.removeFish.bind(this);
 
 		// set initial state
 		this.state = {
@@ -61,6 +64,20 @@ class App extends React.Component {
 		// you could do { fishes }
 	}
 
+	updateFish(key, updatedFish) {
+		const fishes = {...this.state.fishes};
+		fishes[key] = updatedFish;
+		this.setState({ fishes });
+		// ({ fishes: fishes })
+	}
+
+	removeFish(key) {
+		const fishes = {...this.state.fishes}
+		// delete does not work with firebase
+		fishes[key] = null;
+		this.setState({ fishes });
+	}
+
 	loadSamples() {
 		this.setState({
 			fishes: sampleFishes
@@ -76,6 +93,12 @@ class App extends React.Component {
 		// could be { order: order }
 	}
 
+	removeFromOrder(key) {
+		const order = {...this.state.order};
+		delete order[key];
+		this.setState({ order });
+	}
+
 	render () {
 		return (
 			<div className="catch-of-the-day">
@@ -89,8 +112,19 @@ class App extends React.Component {
 						}
 					</ul>
 				</div>
-				<Order fishes={this.state.fishes} order={this.state.order} params={this.props.params} />
-				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
+				<Order 
+					fishes={this.state.fishes} 
+					order={this.state.order} 
+					params={this.props.params}
+					removeFromOrder={this.removeFromOrder}
+				/>
+				<Inventory 
+					addFish={this.addFish} 
+					loadSamples={this.loadSamples} 
+					fishes={this.state.fishes}
+					updateFish={this.updateFish}
+					removeFish={this.removeFish}
+				/>
 			</div>
 		);
 	}
--- src/components/Order.js
+++ src/components/Order.js
@@ -13,15 +13,17 @@ class Order extends React.Component {
 		{/* not specific to react. we made this up */}
 		const fish = this.props.fishes[key];
 		const count = this.props.order[key];
+		const removeButton = <button onClick={(e) => this.props.removeFromOrder(key)}>&times;</button>
 
 		if(!fish || fish.status === 'unavailable') {
-			return <li key={key}>Sorry, {fish ? fish.name : 'fish'} no longer avaiable</li>
+			return <li key={key}>Sorry, {fish ? fish.name : 'fish'} no longer avaiable {removeButton}</li>
 		}
 
 		return (
 			<li key={key}>
 				<span>{count}lbs {fish.name}</span>
 				<span className="price">{formatPrice(count * fish.price)}</span>
+				{removeButton}
 			</li>
 		)
 	}
